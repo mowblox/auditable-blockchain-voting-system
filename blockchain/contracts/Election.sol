130591580bf7eac29a5ff37b36286cb15bef586b
AngeloKwakye <dangelokwakye@gmail.com>
2024-08-16 10:08:23 +0000

    struct definitions(candidate struct and voter struct)

--- blockchain/contracts/Election.sol
+++ blockchain/contracts/Election.sol
@@ -7,4 +7,19 @@ contract Election {
     bool public isPublic;
     string public start_date;
     string public end_date;
+
+    // variable for keeping the number of candidates in election, increases when a new candidate is added
+    uint public candidatesCount;
+
+    //defines structure for a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    // defines structure for a voter
+    struct Voter {
+        bool hasVoted;
+        uint candidateId;
+    }
 }
