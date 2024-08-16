5d191924e3a4f0001caf592fca23b10363b0a31a
AngeloKwakye <dangelokwakye@gmail.com>
2024-08-16 10:06:43 +0000

    contract declaration and state variables

---
+++ blockchain/contracts/Election.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+contract Election {
+    string public title;
+    string public description;
+    bool public isPublic;
+    string public start_date;
+    string public end_date;
+}
