5bb52d8eed9ad05301af531e26ffea9ce69c4271
AngeloKwakye <dangelokwakye@gmail.com>
2024-08-16 10:29:37 +0000

    constructor function to initialize the election with metadate and dates

--- blockchain/contracts/Election.sol
+++ blockchain/contracts/Election.sol
@@ -27,4 +27,22 @@ contract Election {
     mapping(uint => Candidate) public candidates;
     // also to tie/map a unique blockchain adresss to a voter
     mapping(address => Voter) public voters;
+
+    // event to be emitted when a voter casts a vote
+    event VoteCast(address indexed voter, uint indexed candidateId);
+
+    constructor(
+        string memory _title,
+        string memory _description,
+        bool _isPublic,
+        string _start_date,
+        string _end_date,
+    ) {
+        require(_startDate < _endDate, "Start date must be before end date");
+        title = _title;
+        description = _description;
+        isPublic = _isPublic;
+        start_date = _start_date;
+        end_date = _end_date;
+    }
 }
