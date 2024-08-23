a3bd480ec5e2afa9e9ab47cb8932a4ea06db3c7d
Dennis Anim <150798169+theGeniusDennis@users.noreply.github.com>
2024-08-23 08:46:08 +0000

    Election factory  (#24)
    * Created ElectionFactory.sol file
    
    * Created the basic functions for the Election Factory contract
    
    * Created a function to delete an election
    
    * Created an onlyOwner modifier
    
    * Done with the ElectionFactory.sol file
    
    * Updated code
    
    * Changd the indentation of the file

---
+++ blockchain/contracts/ElectionFactory.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.24;
+
+import "./Election.sol";
+
+contract ElectionFactory {
+    address owner;
+    
+    //Array to keep track of the elections
+    address[] elections;
+
+    constructor (){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+
+    //Event emitted when an election is created
+    event ElectionCreated(address electionAddress);
+
+    //Function to create new election
+    function createElection(
+        string memory _title,
+        string memory _description,
+        bool _isPublic,
+        uint _startDate,
+        uint _endDate
+    ) public {
+        //Create a new instance of the Election contract
+        Election newElection = new Election(
+            _title,
+            _description,
+            _isPublic,
+            _startDate,
+            _endDate
+        );
+
+        //Store the address of the newly created election
+        elections.push(address(newElection));
+
+        //Emit an event for the creation of the new contract
+        emit ElectionCreated(address(newElection));
+    }
+    
+
+    //function to get addresses of all elections
+    function getElections() public view returns (address[] memory){
+        return elections;
+    }
+
+    //function to delete an election
+    function deleteElection(uint _electionID) public onlyOwner {
+        delete elections[_electionID];
+    }
+
+    //Function to retrieve the total number of elections
+    function getTotalElections() public view returns (uint) {
+        return elections.length;
+    }
+}
