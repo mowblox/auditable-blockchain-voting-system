16a0bc3adaaf957483de066b7917f583a6421bf3
mickeymond <33749130+mickeymond@users.noreply.github.com>
2024-09-01 19:03:32 +0000

    Changes to merge PR

--- blockchain/contracts/ElectionFactory.sol
+++ blockchain/contracts/ElectionFactory.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
-import "hardhat/console.sol";
 import "./Election.sol";
 
 contract ElectionFactory {
--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,6 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@typechain/hardhat"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
