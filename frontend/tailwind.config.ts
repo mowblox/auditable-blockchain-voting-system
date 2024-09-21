dcb878a3788e355cef02a19e2c2a5f1ec7a1dd0d
mickeymond <33749130+mickeymond@users.noreply.github.com>
2024-09-21 20:31:22 +0000

    Installing & Setting Up FlowBite React

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+import flowbite from "flowbite-react/tailwind";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content()
   ],
   theme: {
     colors: {
@@ -22,6 +24,8 @@ const config: Config = {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin()
+  ],
 };
 export default config;
