2ff7abad6ab4ad4f33eedb4a75ec10c485236ac7
Wilbert Boadzo Elekplim <118719880+Webnai@users.noreply.github.com>
2024-10-19 12:18:27 +0000

    Feature/election list view (#67)
    * all elections view page, also responsive on all devices
    
    * filter button fix
    
    * search and filter election by type functionalities implemented

--- frontend/src/app/elections/layout.tsx
+++ frontend/src/app/elections/layout.tsx
@@ -7,7 +7,7 @@ export default function ElectionsLayout({
 }>) {
   return (
     <div className="h-full px-4 lg:px-0 text-text">
-      <div className="md:w-[80%] m-auto flex flex-col lg:flex-row mt-14 gap-14">
+      <div className="md:w-[90%] m-auto flex flex-col lg:flex-row mt-14 gap-14">
         <SideBarWrapper />
         <div className="mt-8 lg:mt-0 lg:ml-10 w-full lg:w-full">
           {children}
--- frontend/src/components/elections/AllElections.tsx
+++ frontend/src/components/elections/AllElections.tsx
@@ -1,43 +1,127 @@
-'use client';
-import { ELECTION_FACTORY_ABI, getFactoryAddress } from "@/contracts/ElectionFactory";
+"use client";
+import {
+  ELECTION_FACTORY_ABI,
+  getFactoryAddress,
+} from "@/contracts/ElectionFactory";
 import { useSDK } from "@metamask/sdk-react";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import Web3 from "web3";
-import ElectionTitle from "./ElectionTitle";
+import searchIcon from "../../../public/images/search-icon.svg";
+import Image from "next/image";
 import ElectionDescription from "./ElectionDescription";
+import rectangle from "../../../public/images/Rectangle 7.svg";
+import { ELECTION_ABI } from "@/contracts/Election";
+
+interface Election {
+  address: string;
+  title: string;
+  type: string; 
+}
 
 export default function AllElections() {
-  const [elections, setElections] = useState([]);
+  const [elections, setElections] = useState<Election[]>([]);
   const { provider, connected, chainId } = useSDK();
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchType, setSearchType] = useState("all");
 
   useEffect(() => {
-    // Call transaction
-    if (connected && provider) {
-      // Initialize web3
-      const web3 = new Web3(provider);
-      // Initialize contract
-      const electionFactory = new web3.eth.Contract(ELECTION_FACTORY_ABI, getFactoryAddress(provider.getChainId()));
-      // Invote method
-      electionFactory
-        .methods
-        .getElections()
-        .call()
-        .then((addresses) => setElections(addresses as []))
-        .catch(console.log);
-    }
-  }, [provider, connected, chainId])
+    const fetchElections = async () => {
+      if (connected && provider) {
+        const web3 = new Web3(provider);
+        const electionFactory = new web3.eth.Contract(
+          ELECTION_FACTORY_ABI,
+          getFactoryAddress(provider.getChainId())
+        );
+
+        try {
+          const addresses = await electionFactory.methods.getElections().call();
+
+          // Fetch titles for each election
+          const fetchedElections = await Promise.all(
+            addresses.map(async (address: string) => {
+              const election = new web3.eth.Contract(ELECTION_ABI, address);
+              const title = await election.methods.title().call();
+
+              return {
+                address,
+                title, // Now using real titles fetched from the contract
+                type: Math.random() > 0.5 ? "public" : "private", // Dummy type, update as necessary
+              };
+            })
+          );
+
+          setElections(fetchedElections);
+        } catch (error) {
+          console.error("Error fetching elections:", error);
+        }
+      }
+    };
+
+    fetchElections();
+  }, [provider, connected, chainId]);
+
+  // Filter elections based on search term and type
+  const filteredElections = elections.filter((election) => {
+    const matchesSearchTerm = election.title
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    const matchesSearchType =
+      searchType === "all" || election.type === searchType;
+
+    return matchesSearchTerm && matchesSearchType;
+  });
+
 
   return (
-    <div className="grid grid-cols-1 gap-4">
-      {elections.map(address => (
-        <Link href={`/elections/${address}`} key={address}>
-          <div className="h-20 overflow-clip">
-            <h1 className="text-2xl"><ElectionTitle address={address} /></h1>
-            <p><ElectionDescription address={address} /></p>
-          </div>
-        </Link>
-      ))}
+    <div className="flex flex-col gap-5 w-full">
+      <div className="flex flex-col md:flex-row items-center gap-4 mb-5">
+        <div className="relative w-full md:w-[90%]">
+          <Image
+            src={searchIcon}
+            alt="Search Icon"
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+          />
+          <input
+            type="text"
+            placeholder="Search for election eg. SRC 2024 election"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="pl-10 px-4 py-2 rounded-3xl focus:outline-none focus:ring-2 focus:ring-blue-600 w-full bg-[#1B1A23] h-11"
+          />
+        </div>
+        
+        <select
+          value={searchType}
+          onChange={(e) => setSearchType(e.target.value)}
+          className="py-2 px-4 h-11 w-[80%] md:w-[18%] lg:w-[12%] rounded-3xl focus:outline-none focus:ring-2 focus:ring-blue-600 bg-gradient-to-r from-primary to-[#4595DF] hover:from-[#4595DF] hover:to-primary cursor-pointer transition-all duration-300 ease-in-out"
+        >
+          <option value="all">All</option>
+          <option value="public">Public</option>
+          <option value="private">Private</option>
+        </select>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7">
+        {filteredElections.map((election) => (
+          <Link href={`/elections/${election.address}`} key={election.address}>
+            <div className="grid col-span-1 overflow-clip bg-[#1B1A23] rounded-xl p-6 h-72 mb-3 bg-gradient-to-r hover:from-[#4595DF] hover:to-primary transition-all duration-800 ease-in-out group leading-tight">
+              <h1 className="text-2xl font-space-grotesk font-bold group-hover:text-[#fff]">
+                {election.title} {/* Now the real election title */}
+              </h1>
+              <Image src={rectangle as any} alt="Rectangle" />
+              <p className="text-subtle-text group-hover:text-[#fff]">
+                <ElectionDescription address={election.address} />
+              </p>
+              <div className="flex items-end justify-end">
+                <button className="bg-dark group-hover:bg-text group-hover:text-dark rounded-3xl py-2 px-4 mt-4 group-hover:font-bold">
+                  See Details
+                </button>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
