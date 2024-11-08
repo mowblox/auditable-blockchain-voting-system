b4b9d703bc2ed77bb2e7abcd3fd544dd93eeb26e
Wilbert Boadzo <wilbertboadzo144@gmail.com>
2024-11-08 17:42:02 +0000

    removing magic components

--- frontend/src/components/WalletProvider.tsx
+++ frontend/src/components/WalletProvider.tsx
@@ -1,41 +1,27 @@
+"use client";
 import '@rainbow-me/rainbowkit/styles.css';
-import { darkTheme, RainbowKitProvider, connectorsForWallets } from '@rainbow-me/rainbowkit';
-import { createConfig} from 'wagmi';
+import { darkTheme, getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { sepolia, scrollSepolia } from 'wagmi/chains';
-import { rainbowMagicConnector } from './RainbowMagicConnector';
-import { WagmiProvider } from 'wagmi';
 
 const queryClient = new QueryClient();
 
-const chains = [sepolia, scrollSepolia];
-
-const connectors = connectorsForWallets(
-  [
-    {
-      groupName: 'Recommended',
-      wallets: [rainbowMagicConnector({ chains }) as any],
-    }
-  ],
-  {
-    projectId: 'your_project_id', 
-    appName: 'TrueCast',
-  }
-);
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,         
-  chains: [sepolia, scrollSepolia],  
+const config = getDefaultConfig({
+  appName: 'Auditable Voting Dapp',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [sepolia, scrollSepolia]
 });
 
 export default function WalletProvider({
   children,
-}: { children: React.ReactNode }) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <WagmiProvider config={wagmiConfig}>
+    <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider chains={chains} theme={darkTheme()}>
+        <RainbowKitProvider theme={darkTheme()}>
           {children}
         </RainbowKitProvider>
       </QueryClientProvider>
