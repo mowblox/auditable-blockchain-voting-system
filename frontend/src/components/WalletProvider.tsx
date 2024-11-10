febbaa4e0944e0870943f735644cde3d767448f0
Wilbert Boadzo Elekplim <118719880+Webnai@users.noreply.github.com>
2024-11-10 12:38:52 +0000

    rainbow magic connector (#84)
    * rainbow magic connector
    
    * using web3auth instead of magicfor account abstraction
    
    * updated example .env
    
    * deploy on mowblox/truecast
    
    ---------
    
    Co-authored-by: mickeymond <33749130+mickeymond@users.noreply.github.com>

--- frontend/src/components/WalletProvider.tsx
+++ frontend/src/components/WalletProvider.tsx
@@ -1,19 +1,32 @@
 "use client";
 import '@rainbow-me/rainbowkit/styles.css';
 import { darkTheme, getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
+import { rainbowWeb3AuthConnector } from "./RainbowWeb3AuthConnector";
+import { WagmiProvider, http } from 'wagmi';
+import { rainbowWallet, metaMaskWallet } from '@rainbow-me/rainbowkit/wallets';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { sepolia, scrollSepolia } from 'wagmi/chains';
+import { sepolia, scrollSepolia, mainnet } from 'wagmi/chains';
 
 const queryClient = new QueryClient();
 
 const config = getDefaultConfig({
-  appName: 'Auditable Voting Dapp',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [sepolia, scrollSepolia]
+  appName: 'TrueCast',
+  projectId: process.env.NEXT_PUBLIC_MAGIC_API_KEY || "", 
+  chains: [mainnet, sepolia, scrollSepolia],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+    [scrollSepolia.id]: http(),
+  },
+  wallets: [{
+    groupName: 'Recommended',
+    wallets: [
+      rainbowWallet,
+      rainbowWeb3AuthConnector,
+      metaMaskWallet,
+    ],
+  }],
 });
-
-
 
 export default function WalletProvider({
   children,
